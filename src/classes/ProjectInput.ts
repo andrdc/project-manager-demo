1c8df30ef481bcafcfe41e06a9ae45a3f988d1bf
Andres Ruiz <andrsruiz@gmail.com>
2021-11-30 13:07:55 -0600

    submit event

--- src/classes/ProjectInput.ts
+++ src/classes/ProjectInput.ts
@@ -2,6 +2,9 @@ export default class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,10 +12,27 @@ export default class ProjectInput {
 
 		const node = document.importNode(this.templateElement.content, true);
 		this.element = node.firstElementChild as HTMLFormElement;
-		this.attach();
+		this.element.id = 'user-input';
+
+		this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+		this.Configure();
+		this.Attach();
 	}
 
-	private attach() {
+	private SubmitHandler(event: Event) {
+		event.preventDefault();
+		console.debug(event);
+		console.debug(this);
+	}
+
+	private Configure() {
+		this.element.addEventListener('submit', this.SubmitHandler.bind(this));
+	}
+
+	private Attach() {
 		this.hostElement.insertAdjacentElement('beforeend', this.element);
 	}
 }
