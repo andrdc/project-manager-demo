ca68f906f416142aba6d22e818e92ab608625288
Andres Ruiz <andrsruiz@gmail.com>
2021-12-01 14:39:04 -0600

    Validate User Input

--- src/classes/ProjectInput.ts
+++ src/classes/ProjectInput.ts
@@ -1,3 +1,5 @@
+import Validatable from "./Validatable";
+
 function Autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
 	const originalMethod = descriptor.value;
 	return {
@@ -33,31 +35,54 @@ class ProjectInput {
 		this.Attach();
 	}
 
-	@Autobind
-	private SubmitHandler(event: Event) {
-		event.preventDefault();
-		this.GatherUserInput();
-		this.CleanInputs();
-	}
-
-	private CleanInputs() {
-		this.titleInputElement.value = '';
-		this.descriptionInputElement.value = '';
-		this.peopleInputElement.value = '';
-	}
-
-	private GatherUserInput() {
-		console.info(this.titleInputElement.value);
-		console.info(this.descriptionInputElement.value);
-		console.info(this.peopleInputElement.value);
-	}
-
 	private Configure() {
 		this.element.addEventListener('submit', this.SubmitHandler);
 	}
 
 	private Attach() {
 		this.hostElement.insertAdjacentElement('beforeend', this.element);
+	}
+
+	@Autobind
+	private SubmitHandler(event: Event) {
+		event.preventDefault();
+		this.GatherUserInput();
+	}
+
+	private GatherUserInput() {
+		const title = new Validatable(
+			this.titleInputElement.value,
+			true,
+			5,
+			20
+		);
+		const description = new Validatable(
+			this.descriptionInputElement.value,
+			true,
+			10,
+			50
+		);
+		const people = new Validatable(
+			+this.peopleInputElement.value,
+			true,
+			1,
+			5
+		);
+
+		if (title.IsValid() && description.IsValid() && people.IsValid()) {
+			console.info(this.titleInputElement.value);
+			console.info(this.descriptionInputElement.value);
+			console.info(this.peopleInputElement.value);
+			this.ClearInputs();
+		} else {
+			alert('Invalid input');
+		}
+	}
+
+	private ClearInputs() {
+		this.titleInputElement.value = '';
+		this.descriptionInputElement.value = '';
+		this.peopleInputElement.value = '';
 	}
 }
 
---
+++ src/classes/Validatable.ts
@@ -0,0 +1,51 @@
+class Validatable {
+	private value?: string | number;
+	private required?: boolean;
+	private minLength?: number;
+	private maxLength?: number;
+	private min?: number;
+	private max?: number;
+
+	constructor(value: number, required?: boolean, min?: number, max?: number);
+	constructor(value: string, required?: boolean, min?: number, max?: number);
+	constructor(value?: any, required?: boolean, min?: number, max?: number) {
+		if (typeof value === 'string') {
+			this.value = value;
+			this.required = required;
+			this.minLength = min;
+			this.maxLength = max;
+		} else if (typeof value === 'number') {
+			this.value = value;
+			this.required = required;
+			this.min = min;
+			this.max = max;
+		}
+	}
+
+	IsValid(): boolean {
+		let isValid = true;
+
+		if (typeof this.value === 'string') {
+			if (this.required) {
+				isValid = isValid && this.value.trim().length !== 0;
+			}
+			if (this.minLength != null) {
+				isValid = isValid && this.value.length > this.minLength;
+			}
+			if (this.maxLength != null) {
+				isValid = isValid && this.value.length < this.maxLength;
+			}
+		} else if (typeof this.value === 'number') {
+			if (this.min != null) {
+				isValid = isValid && this.value > this.min;
+			}
+			if (this.max != null) {
+				isValid = isValid && this.value < this.max;
+			}
+		}
+
+		return isValid;
+	}
+}
+
+export default Validatable;
